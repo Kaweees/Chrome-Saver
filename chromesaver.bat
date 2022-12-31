c0cef11fa7d705e228fecf134dac2fea808b5935
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-12-30 16:32:04 -0800

    Update chromesaver.bat

--- chromesaver.bat
+++ chromesaver.bat
@@ -1,4 +1,5 @@
+@echo off
 taskkill /F /IM chrome.exe /T > nul
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set myDate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set myTime=%%a%%b)
-xcopy /s "C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Sessions" "%CD%\Sessions\%myDate%-%myTime%"
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set myTime=%%a%%b)
+Xcopy /s "C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Sessions" "%CD%\Sessions\%myDate%-%myTime%" /E /H /C /I
