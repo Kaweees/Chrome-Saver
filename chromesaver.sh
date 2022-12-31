0dc7fed3f2274b300d7e2c63d867ea1d5dfddff8
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-12-30 21:54:19 -0800

    Update chromesaver.sh

--- chromesaver.sh
+++ chromesaver.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/env bash
 pkill chrome
+$myDate = date +%Y-%m-%d
+$myTime = date +%H-%M
+$scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+rsync -avz /from/C:/Users/$USER/AppData/Local/Google/Chrome/User Data/Default/Sessions/ /to/$scriptDir/Sessions/$myDate-$myTime
