a6d58cc2082f9c2058f88863dd344f0c37b882e4
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-12-30 21:27:09 -0800

    Update chromesaver.ps1

--- chromesaver.ps1
+++ chromesaver.ps1
@@ -1,3 +1,4 @@
-get-process -name Chrome | stop-process
-$myDate = [datetime]::now.tostring("MM-dd-yy")
+Stop-Process -Name Chrome
+$myDate = [datetime]::now.tostring("yy-MM-dd")
 $myTime = [datetime]::now.tostring("hhmm")
+Copy-Item -Path "C:\Users\$env:UserName\AppData\Local\Google\Chrome\User Data\Default\Sessions\" -Destination "$PSScriptRoot\Sessions\$myDate-$myTime" -Recurse
